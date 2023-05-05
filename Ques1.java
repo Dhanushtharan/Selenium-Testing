037f55c5fcd6d4fd11c78e0696a25fdb2cd61f51
Dhanush Tharan M <106361969+Dhanushtharan@users.noreply.github.com>
2023-05-05 09:42:54 +0530

    Add files via upload

---
+++ Ques1.java
@@ -0,0 +1,64 @@
+package CA_1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Ques1 {
+    public static void main( String[] args ) throws InterruptedException
+    {
+    	ChromeOptions co = new ChromeOptions();
+    	co.addArguments("--remote-allow-origins=*");
+    	WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(co);
+        driver.get("https://www.amazon.in/Amazfit-Stratos-A1619-Multisport-Smartwatch/dp/B07BTKVG7S/ref=sr_1_2?qid=1683196286&s=computers&sr=1-2&srs=12516989031");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        String etitle = "Amazfit Stratos A1619 Multisport Smartwatch (Black) : Amazon.in: Computers & Accessories";
+        System.out.println(title);
+        if(title.equals(etitle)) {
+        	System.out.println("Title is matching");
+        }
+        else {
+        	System.out.println("Title Mismatch");
+        }
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
+        Thread.sleep(5000); 
+        Boolean present = driver.findElement(By.xpath("//*[@id=\"attach-close_sideSheet-link\"]")).isDisplayed();
+        System.out.println("Is item added to cart:" + present);
+        driver.get("https://www.amazon.in/gp/cart/view.html/ref=dp_atch_dss_cart?");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@value=\"Delete\"]")).click();
+        Thread.sleep(2000);
+        String removeText = driver.findElement(By.xpath("//*[@id=\"sc-active-cart\"]/div/div[1]/div/h1")).getText();
+        String extext = "Your Amazon Cart is empty.";
+        if(removeText.equals(extext)) {
+        	System.out.println("Product is removed");
+        }
+        driver.get("https://www.amazon.in/Huami-Amazfit-Smart-Watch-Desert/dp/B07XTSH7JX/ref=sr_1_1?qid=1683196286&s=computers&sr=1-1&srs=12516989031");
+        driver.findElement(By.xpath("//*[@id=\"add-to-cart-button\"]")).click();
+        Thread.sleep(2000);
+        driver.get("https://www.amazon.in/gp/cart/view.html/ref=dp_atch_dss_cart?");
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"a-autoid-0-announce\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"quantity_2\"]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@name=\"proceedToRetailCheckout\"] "));
+        Thread.sleep(1000);
+        String sign = driver.getTitle();
+        System.out.println(sign);
+        String esign = "Amazon Sign In";
+        if(sign.equals(esign)) {
+        	System.out.println("Success");
+        }
+        else {
+        	System.out.println("Failure");
+        }
+    }
+}
+
+
+
+
